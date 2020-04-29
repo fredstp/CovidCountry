1e5479643b5b4e6be62e07f2335cdcc65dea63f8
Ludovic Dureau <ludodureau@gmail.com>
2020-04-29 20:08:28 +0200

    random video working

--- src/Controller/WindyController.php
+++ src/Controller/WindyController.php
@@ -7,10 +7,15 @@ class WindyController extends AbstractController
 {
     public function cam()
     {
-        $camData = $this->get('https://api.windy.com/api/webcams/v2/?key=' . APP_API_KEY);
-
+        $camData = $this->get('https://api.windy.com/api/webcams/v2/list/country=IT?key=' . APP_API_KEY);
+        $webcams = $camData['result']['webcams'];
+        $camData = $webcams[rand(0, count($webcams) - 1)];
+        $webcam = $this->get('https://api.windy.com/api/webcams/v2/list/webcam=' . $camData['id'] . '?show=webcams:image,location,player&key=' . APP_API_KEY);
+        $iframeSrc = $webcam['result']['webcams'][0]['player']['lifetime']['embed'];
+        //var_dump($iframeSrc); die;
         return $this->twig->render('Windy/cam.html.twig', [
-            'cam_data' => $camData,
+            'iframe_src' => $iframeSrc,
+            'api_key'  => APP_API_KEY,
         ]);
     }
 }
