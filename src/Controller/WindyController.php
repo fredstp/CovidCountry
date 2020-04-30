0aa2e2f24a8f16bb22d41a83548e324c998a579f
Ludovic Dureau <ludodureau@gmail.com>
2020-04-30 11:04:05 +0200

    back mostly done rdm country in front

--- src/Controller/WindyController.php
+++ src/Controller/WindyController.php
@@ -7,12 +7,14 @@ class WindyController extends AbstractController
 {
     public function cam()
     {
-        $camData = $this->get('https://api.windy.com/api/webcams/v2/list/country=FR?key=' . APP_API_KEY);
+        $camData = $this->get('https://api.windy.com/api/webcams/v2/list/?key=' . APP_API_KEY);
+
         $webcams = $camData['result']['webcams'];
         $camData = $webcams[rand(0, count($webcams) - 1)];
-        $webcam = $this->get('https://api.windy.com/api/webcams/v2/list/webcam=' . $camData['id'] .
-                             '?show=webcams:image,location,player&key=' . APP_API_KEY);
+        $webcam = $this->get('https://api.windy.com/api/webcams/v2/list/webcam=' . $camData['id']
+            . '?show=webcams:image,location,player&key=' . APP_API_KEY);
         $iframeSrc = $webcam['result']['webcams'][0]['player']['lifetime']['embed'];
+
         return $this->twig->render('Windy/cam.html.twig', [
             'iframe_src' => $iframeSrc,
             'api_key'  => APP_API_KEY,
@@ -21,6 +23,11 @@ class WindyController extends AbstractController
 
     public function country()
     {
+        $win = false;
+        if (isset($_GET['win']) && $_GET['win'] == 1) {
+            $win = true;
+        }
+
         $countries = [
             'france' => 1228218512,
             'italie' => 1230041254,
@@ -34,24 +41,45 @@ class WindyController extends AbstractController
             'suisse' => 1292514349,
 
         ];
-        $webcams = [];
-        foreach ($countries as $country => $id) {
-            $webcam = $this->get('https://api.windy.com/api/webcams/v2/list/webcam=' . $id .
-                '?show=webcams:image,location,player&key=' . APP_API_KEY);
-            $webcams[$country] = $webcam['result']['webcams'][0]['player']['lifetime']['embed'];
+        $this->shuffleAssoc($countries);
+        $country = '';
+        $id      = 0;
+        foreach ($countries as $key => $value) {
+            $country = $key;
+            $id      = $value;
+            break;
         }
+        $webcam = $this->get('https://api.windy.com/api/webcams/v2/list/webcam=' . $id .
+            '?show=webcams:image,location,player&key=' . APP_API_KEY);
+        $iframeSrc = $webcam['result']['webcams'][0]['player']['lifetime']['embed'];
 
         return $this->twig->render('Windy/country.html.twig', [
-            'webcams' => $webcams,
+            'iframe_src' => $iframeSrc,
+            'country'    => $country,
+            'win'        => $win,
 
         ]);
     }
 
-    public function error() {
-        if (isset($_POST['validation'])) {
-            if (empty($_POST['answer'])) {
-                echo 'Et la réponse alors ?';
-            }
+    private function shuffleAssoc(&$array)
+    {
+        $keys = array_keys($array);
+        shuffle($keys);
+
+        $new = [];
+        foreach ($keys as $key) {
+            $new[$key] = $array[$key];
         }
+        $array = $new;
+        return true;
+    }
+    public function check()
+    {
+        if ($_POST['answer'] === $_POST['country']) {
+            $win = true;
+        } else {
+            $win = false;
+        }
+        header('Location: /windy/country/?win=' . $win);
     }
 }
